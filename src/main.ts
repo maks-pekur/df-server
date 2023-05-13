5bdfd421894fba29010a31338a248790e7919c2e
Maks Pekur <maxpekur88@gmail.com>
2023-05-13 16:08:59 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   // app.enableCors();
-  app.use(
-    session({
-      secret: 'keyword',
-      resave: false,
-      saveUninitialized: false,
-    }),
-  );
+  // app.use(
+  //   session({
+  //     secret: 'keyword',
+  //     resave: false,
+  //     saveUninitialized: false,
+  //   }),
+  // );
   app.use(passport.initialize());
-  app.use(passport.session());
+  // app.use(passport.session());
   await app.listen(5000);
 }
 bootstrap();
