738a3da704756293ee1139eda23153ce9e5b860f
Maks Pekur <maxpekur88@gmail.com>
2023-05-13 15:15:55 +0200

    fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MulterModule } from '@nestjs/platform-express';
 import { AuthModule } from './auth/auth.module';
 import { CartModule } from './cart/cart.module';
 import { CategoriesModule } from './categories/categories.module';
+import { PopularsModule } from './populars/populars.module';
 import { ProductsModule } from './products/products.module';
+import { StoriesModule } from './stories/stories.module';
 import { UploadsModule } from './uploads/uploads.module';
 import { UsersModule } from './users/users.module';
-import { PopularsModule } from './populars/populars.module';
-import { StoriesModule } from './stories/stories.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://maxpekur:cluster@cluster.torlhhm.mongodb.net/?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     MulterModule.register({
       dest: './files',
     }),
